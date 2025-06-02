7ec188ee3b6b590af02a6fc864f7816c2c80ec01
Rebecca <rebecca.grevens@gmail.com>
2025-06-02 12:50:37 +0200

    fixed plot and uniprot accession of ncbi fasta file

--- data_preprocess/disease.py
+++ data_preprocess/disease.py
@@ -1,37 +1,32 @@
 # get disease information
 
-import os
-import requests
-from Bio import SeqIO
 import csv
 from io import StringIO
 from collections import Counter
+import re
+import requests
+from Bio import SeqIO
 import matplotlib.pyplot as plt
-
-
-GLYCO_DIR = '../data/glycosylation'
-S_NITRO_DIR = '../data/s_nitrosylation'
-ACET_DIR = '../data/acetylation'
 
 
 def get_ids(filepath):
     uniprot_ids = []
+    other_records = []
     count = 0
     for record in SeqIO.parse(filepath, "fasta"):
         if record.id.startswith("sp|") or record.id.startswith("tr|"):
-            uniprot_ids.append(record.id.split("|")[1])
+            name = record.id.split("|")[1]
+            if name[-2] == '.':
+                name = name[:-2]
+            uniprot_ids.append(name)
         else:
             count += 1
+            other_records.append(record)
     print(f"{count} sequences not in Uniprot")
-    return uniprot_ids
+    return uniprot_ids, other_records
 
 
 def get_records(uniprot_ids):
-    filepath = "./disease.fasta"
-    if os.path.exists(filepath):
-        os.remove(filepath)
-
-    url = "https://rest.uniprot.org/uniprotkb/stream"
     chunk_size = 500
     records = set()
     for i in range(0, len(uniprot_ids), chunk_size):
@@ -39,51 +34,54 @@ def get_records(uniprot_ids):
         query = " OR ".join([f"accession:{uid}" for uid in chunk])
         fields = "accession,id,cc_disease,sequence"
         url = f"https://rest.uniprot.org/uniprotkb/stream?format=tsv&query={query}&fields={fields}"
-        
+
         response = requests.get(url, timeout=30)
         if response.ok:
             tsv_data = StringIO(response.text)
             reader = csv.DictReader(tsv_data, delimiter='\t')
             for row in reader:
                 acc = row.get("Entry", "")
-                id = row.get("Entry Name", "")
+                name = row.get("Entry Name", "")
                 disease = row.get("Involvement in disease", "")
+                mim_ids = re.findall(r'\[MIM:(\d+)', disease)
+                mim_string = ""
+                for m in mim_ids:
+                    mim_string += f"MIM:{m}"
                 seq = row.get("Sequence", "")
-                stuff = [acc,id,disease,seq]
-                records.add(tuple(stuff))
-                """with open(filepath, "a") as f:
-                    f.write(f">sp|{acc}|{id} {disease}\n{seq}\n")"""
+                record = [acc,name,mim_string,seq]
+                records.add(tuple(record))
         else:
             print("(Disease) Batch download failed:", response.status_code)
 
     return records
 
 
-def vis_disease(filepath):
+def vis_disease(dir_path):
     diseases = []
-    for record in SeqIO.parse(f"{filepath}/merged_d.fasta", "fasta"):
-        if "DISEASE" in record.description:
-            parts = record.description.split("DISEASE: ")
+    for record in SeqIO.parse(f"{dir_path}/merged.fasta", "fasta"):
+        if "MIM:" in record.description:
+            parts = record.description.split("MIM:")
             for disease_part in parts[1:]:
                 diseases.append(disease_part)
 
     counts = Counter(diseases)
     top10 = counts.most_common(10)
-    labels = [d[:20] for d, c in top10]
+    labels = [d for d, c in top10]
     values = [c for d, c in top10]
     plt.bar(labels, values, color='skyblue', edgecolor='black')
-    plt.xlabel('Diseases')
+    plt.xlabel('MIM ID of Diseases')
     plt.ylabel('Absolute Frequency')
     plt.xticks(rotation=45, ha='right')
-    plt.title(f'Absolute Frequency of Top 10 Diseases (PTM: {filepath.split("/")[-1]})')
+    plt.title(f'Absolute Frequency of Top 10 Diseases (PTM: {dir_path.split("/")[-1]})')
     plt.tight_layout()
-    plt.savefig(f"{filepath}/top10diseases.jpg")
+    plt.savefig(f"{dir_path}/top10diseases.jpg")
+    plt.clf()
 
 
 def main(filepath):
-    ids =  get_ids(filepath)
+    ids, other_records =  get_ids(filepath)
     records = get_records(ids)
-    return records
+    return records, other_records
 
 
 if __name__ == '__main__':
--- data_preprocess/merge.py
+++ data_preprocess/merge.py
@@ -2,7 +2,6 @@
 
 import os
 import disease
-from Bio import SeqIO
 
 
 GLYCO_DIR = '../data/glycosylation'
@@ -13,18 +12,19 @@ ACET_DIR = '../data/acetylation'
 def main():
     dirs = [GLYCO_DIR, S_NITRO_DIR, ACET_DIR]
     for d in dirs:
-        merged_file = os.path.join(d, "merged_d.fasta")
+        merged_file = os.path.join(d, "merged.fasta")
         if os.path.exists(merged_file):
             os.remove(merged_file)
 
         records = []
         new_records = []
+        rec_without_disease = []
 
         for filename in os.listdir(d):
-            if filename.endswith(".fasta") and filename != "merged.fasta" and filename != "seqs.fasta" and filename != "merged_d.fasta":
+            if filename.endswith(".fasta") and filename != "merged.fasta" and filename != "seqs.fasta":
                 filepath = os.path.join(d, filename)
-                print(filepath)
-                with_d = disease.main(filepath)
+                with_d, without_d = disease.main(filepath)
+                rec_without_disease.append(without_d)
                 for record in with_d:
                     new_records.append(record)
                 all_records = records + new_records
@@ -37,6 +37,9 @@ def main():
         with open(merged_file, "w") as merged:
             for rec in records:
                 merged.write(f">{rec[0]}|{rec[1]} {rec[2]}\n{rec[3]}\n")
+            for recs in rec_without_disease:
+                for r in recs:
+                    merged.write(f">{r.description}\n{r.seq}\n")
 
         print("Merged succesfully\n")
         disease.vis_disease(d)
