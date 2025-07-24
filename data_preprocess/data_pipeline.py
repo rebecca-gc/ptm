23c94d242b165cdd773d58f4a5daabe3014baa07
Rebecca <rebecca.grevens@gmail.com>
2025-07-24 15:07:40 +0200

    removed 3000

--- data_preprocess/data_pipeline.py
+++ data_preprocess/data_pipeline.py
@@ -19,7 +19,7 @@ def main():
     for dir in os.listdir(ptms_dir):
         dir_path = os.path.join(ptms_dir, dir)
         if os.path.isdir(dir_path):
-            class_generator.generator(os.path.join(dir_path, 'merged3000.fasta'),f'data/no_ptm/filtered3000_no_{dir}.fasta',dir_path,factor=1.5)
+            class_generator.generator(os.path.join(dir_path, 'merged.fasta'),f'data/no_ptm/filtered_no_{dir}.fasta',dir_path,factor=1.5)
             databases_path = os.path.join(dir_path, 'databases')
             for db in os.listdir(databases_path):
                 class_generator.generator(os.path.join(databases_path, db),f'data/no_ptm/filtered3000_no_{dir}.fasta',os.path.join(dir_path, 'db_seqs_classes'),db=f'{dir}-{db.split(".")[0]}_',factor=1.5)
